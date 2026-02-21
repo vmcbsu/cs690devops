0caef3e913c1cb4e1d1d677355cfbdc33f44982a
vmcbsu <vincent.mcconnell@bsu.edu>
2026-02-21 20:22:50 +0000

    cn1

--- FactorialLibrary.Tests/FactorialServiceTests.cs
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -11,4 +11,10 @@ public class FactorialServiceTests
         Assert.Equal(1,result);
 
     }
+    [Fact]
+    public void Test_negative(){
+        Assert.Throws<InvalidOperationException>(()=>{
+            var result = FactorialService.Factorial(-1);
+        });
+    }
 }
