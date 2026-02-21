7482bfa4d3fcaa1fca2cce26ccf18d75d857aec3
vmcbsu <vincent.mcconnell@bsu.edu>
2026-02-21 20:14:30 +0000

    c1

---
+++ FactorialLibrary.Tests/FactorialServiceTests.cs
@@ -0,0 +1,14 @@
+﻿//namespace FactorialLibrary.Tests;
+namespace FactorialService.Tests;
+using FactorialLibrary;
+
+public class FactorialServiceTests
+{
+    [Fact]
+    public void Test1()
+    {
+        var result = FactorialService.Factorial(0);
+        Assert.Equal(1,result);
+
+    }
+}
